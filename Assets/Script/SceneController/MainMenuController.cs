290ee33383a85ed5afe32b8baa0196c6f85bad67
MrGolem <MANbbq65@gmail.com>
2023-02-01 03:24:53 +0200

    UpdatedMainMenuScreen(Graphic Changes v0.1)

--- Assets/Script/SceneController/MainMenuController.cs
+++ Assets/Script/SceneController/MainMenuController.cs
@@ -1,33 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
     public GameObject LeaderboardPanel, SettingsPanel, SavedGamesPanel, SpecialMessage, BestiaryPanel;// всі панельки відкриваються при натисненні на кнопці
 
+    private void Start()
+    {
+        LeaderboardPanel.SetActive(false);
+        SettingsPanel.SetActive(false);
+        SavedGamesPanel.SetActive(false);
+        SpecialMessage.SetActive(false);
+        BestiaryPanel.SetActive(false);
+    }
+
     public void OnClickLeaderboard()
     {
 
     }
+
     public void OnClickSettings()
     {
 
     }
+
     public void OnClickContinue()
     {
 
     }
+
     public void OnClickNewGame()
     {
 
     }
+
     public void OnClickBesteary()
     {
 
     }
+
     public void OnClickExit()
     {
         Application.Quit();
     }
+
 }
